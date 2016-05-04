c908dbd65b7230a86bd63bc107e367baca7a1f62
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2016-05-04 13:16:35 +0200

    Sort submitted jobs by requested time

--- Backfilling.pm
+++ Backfilling.pm
@@ -93,9 +93,10 @@ sub run {
 		$self->reassign_jobs() if (@{$typed_events[JOB_COMPLETED_EVENT]});
 
 		# submission events
-		for my $event (@{$typed_events[SUBMISSION_EVENT]}) {
-			my $job = $event->payload();
+		my @sorted_submission_events = sort {$a->payload()->requested_time() <=> $b->payload()->requested_time()} (@{$typed_events[SUBMISSION_EVENT]});
 
+		for my $event (@sorted_submission_events) {
+			my $job = $event->payload();
 
 			$self->assign_job($job);
 			die "job " . $job->job_number() . " was not assigned" unless defined $job->starting_time();
