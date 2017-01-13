231e963d24637018c38e5aaf3cf1446e795a3b21
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2017-01-13 12:42:18 +0100

    Nothing important

--- Backfilling.pm
+++ Backfilling.pm
@@ -7,6 +7,7 @@ use Exporter qw(import);
 use Time::HiRes qw(time);
 use Data::Dumper;
 use List::Util qw(max min shuffle);
+use Math::Random qw(random_normal);
 
 use Util qw($config);
 use ExecutionProfile;
