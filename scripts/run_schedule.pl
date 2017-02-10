101cc89b3de027d49460c1117ccf7408620b3f0d
Fernando Mendonca <fernando.machado-mendonca@inria.fr>
2017-02-10 12:46:04 +0100

    Update the test files

--- scripts/run_schedule.pl
+++ scripts/run_schedule.pl
@@ -4,6 +4,7 @@ use warnings;
 
 use Data::Dumper qw(Dumper);
 use Config::Simple;
+use Switch;
 
 use Util qw($config get_benchmark_data get_comm_data);
 use Trace;
@@ -20,7 +21,6 @@ $config = Config::Simple->new('test.conf');
 
 my $platform_name = $config->param('parameters.platform_name');
 my @platform_levels = $config->param("$platform_name.platform_levels");
-my @platform_slowdown = $config->param("$platform_name.platform_slowdown");
 
 my $platform = Platform->new(\@platform_levels);
 
@@ -30,13 +30,52 @@ my $jobs_number = $config->param('parameters.jobs_number');
 
 my $trace = Trace->new_from_swf("$traces_path/$swf_filename");
 $trace->remove_large_jobs($platform->processors_number());
-#$trace->reset_jobs_numbers();
+$trace->reset_jobs_numbers();
 $trace->fix_submit_times();
 $trace->keep_first_jobs($jobs_number) if defined $jobs_number;
 
-my $reduction_algorithm = BestEffortPlatform->new($platform);
+my $reduction_algorithm;
 
-my $schedule = Backfilling->new($reduction_algorithm, $platform, $trace);
+switch ($config->param('parameters.variant_name')) {
+	case 'basic' {
+		$reduction_algorithm = Basic->new();
+	}
+
+	case 'becont' {
+		$reduction_algorithm = BestEffortContiguous->new();
+	}
+
+	case 'beloc' {
+		$reduction_algorithm = BestEffortLocal->new($platform);
+	}
+
+	case 'beplat' {
+		$reduction_algorithm = BestEffortPlatform->new($platform);
+	}
+
+	case 'cont' {
+		$reduction_algorithm = ForcedContiguous->new();
+	}
+
+	case 'loc' {
+		$reduction_algorithm = ForcedLocal->new($platform);
+	}
+
+	case 'plat' {
+		$reduction_algorithm = ForcedPlatform->new($platform);
+	}
+}
+
+my $penalty_function = $config->param('parameters.penalty_function');
+my $penalty_function_factor = $config->param('parameters.penalty_function_' . $penalty_function . '_factor');
+
+my $schedule = Backfilling->new(
+	$reduction_algorithm,
+	$platform,
+	$trace,
+	$penalty_function,
+	$penalty_function_factor,
+);
 $schedule->run();
 
 my @results = (
